bf7a40d00c6e9fad5c296565cef25e7c4f88b15a
ayan4m1 <andrew@bulletlogic.com>
2025-12-05 08:13:38 -0500

    feat: set the selected league from the header

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,6 @@
-import { Fragment } from 'react';
+import { Fragment, useCallback, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Form, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faDoorClosed,
@@ -11,10 +11,24 @@ import {
 
 import useLeagues from '../hooks/useLeagues';
 import useAuthContext from '../hooks/useAuthContext';
+import useAppContext from '../hooks/useAppContext';
 
 export default function Heading() {
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const { setSelectedLeague } = useAppContext();
   const { logIn, token } = useAuthContext();
   const { data } = useLeagues();
+  const handleLeagueChange = useCallback(() => {
+    const league = data.leagues.find(
+      (league) => league.id === selectRef.current.value
+    );
+
+    if (!league) {
+      return;
+    }
+
+    setSelectedLeague(league);
+  }, [data, setSelectedLeague]);
 
   return (
     <Container fluid className="g-0">
@@ -32,15 +46,20 @@ export default function Heading() {
         </Nav>
         <Nav className="ms-auto me-4">
           {Boolean(data) && (
-            <Nav.Item>
-              <Form.Select>
-                {data.leagues.map((item) => (
-                  <option key={item.id} value={item.id}>
-                    {item.name}
-                  </option>
-                ))}
-              </Form.Select>
-            </Nav.Item>
+            <Fragment>
+              <Nav.Item>
+                <Form.Select ref={selectRef}>
+                  {data.leagues.map((item) => (
+                    <option key={item.id} value={item.id}>
+                      {item.name}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Nav.Item>
+              <Nav.Item>
+                <Button onClick={handleLeagueChange}>Set</Button>
+              </Nav.Item>
+            </Fragment>
           )}
           <Nav.Link className="nav-item" onClick={() => logIn('test')}>
             {token ? (
