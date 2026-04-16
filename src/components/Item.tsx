5138be46e1ee6d4a7ce5cadc40de8d7dbffe9ba3
ayan4m1 <andrew@bulletlogic.com>
2026-04-15 22:42:15 -0400

    feat: flesh out item pane

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,24 +1,93 @@
-import { Card, Col } from 'react-bootstrap';
+import { useMemo } from 'react';
+import {
+  Card,
+  Col,
+  ListGroup,
+  OverlayTrigger,
+  Row,
+  Tooltip
+} from 'react-bootstrap';
 
-import { Item } from '../types';
+import { Item, ItemFrameType, ItemRarity } from '../types';
 
 interface IProps {
   item: Item;
 }
 
 export default function Item({ item }: IProps) {
+  const color = useMemo(() => {
+    switch (item.rarity) {
+      case ItemRarity.Magic:
+        return '#6e6ed0';
+      case ItemRarity.Rare:
+        return '#ffff81';
+      case ItemRarity.Unique:
+        return '#ae6135';
+      default:
+      case ItemRarity.Normal:
+        return '#fefefe';
+    }
+  }, [item]);
+
+  const slimDisplay = [
+    ItemFrameType.Currency,
+    ItemFrameType.DivinationCard,
+    ItemFrameType.Gem
+  ].includes(item.frameType);
+
   return (
-    <Col className="mb-2" xs={12} sm={4} md={3}>
-      <Card>
-        <Card.Header>
-          <Card.Title>
-            {item.name} {item.typeLine}{' '}
-            {item.stackSize ? `(${item.stackSize})` : null}
+    <Col className="mb-2 d-flex" xs={12} sm={4} md={3}>
+      <Card
+        style={{
+          borderRadius: 8,
+          backgroundColor: 'black',
+          color: 'white',
+          flexGrow: 1
+        }}
+      >
+        <Card.Header style={{ backgroundColor: '#262323' }}>
+          <Card.Title className="text-center" style={{ color }}>
+            <p>
+              <img src={item.icon} />
+            </p>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={(props) => (
+                <Tooltip {...props}>{item?.stashTab?.name}</Tooltip>
+              )}
+            >
+              <p>
+                {item.name} {item.typeLine}{' '}
+                {item.stackSize ? `(${item.stackSize})` : null}
+              </p>
+            </OverlayTrigger>
           </Card.Title>
         </Card.Header>
-        <Card.Body>
-          <img src={item.icon} />
-        </Card.Body>
+        {!slimDisplay && (
+          <Card.Body>
+            <Row className="text-center">
+              <Col xs={12}>
+                {item.ilvl > 0 && <p>Item Level: {item.ilvl}</p>}
+                {Boolean(item.itemLevel) && (
+                  <p>Requires Level {item.itemLevel}</p>
+                )}
+              </Col>
+              <Col xs={12}>
+                <ListGroup>
+                  {item.implicitMods?.map((implicit) => (
+                    <ListGroup.Item>{implicit}</ListGroup.Item>
+                  ))}
+                  {Boolean(
+                    item.implicitMods?.length && item.explicitMods?.length
+                  ) && <hr />}
+                  {item.explicitMods?.map((explicit) => (
+                    <ListGroup.Item>{explicit}</ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </Col>
+            </Row>
+          </Card.Body>
+        )}
       </Card>
     </Col>
   );
