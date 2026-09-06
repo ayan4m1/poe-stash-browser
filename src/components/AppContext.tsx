9bea829a70fbda1e2087b9757d817399fd6155b4
ayan4m1 <andrew@bulletlogic.com>
2026-09-06 18:10:45 -0400

    feat: add savedItems to AppContext

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 import { League, StashTab } from '../types';
 import { AppContext } from '../hooks/useAppContext';
@@ -8,12 +8,23 @@ interface IProps {
 }
 
 export default function AppContextProvider({ children }: IProps) {
+  const [savedItems, setSavedItems] = useState<string[]>(
+    JSON.parse(
+      localStorage.getItem('app.savedItems') ?? '[]'
+    ) as unknown as string[]
+  );
   const [selectedLeague, setSelectedLeague] = useState<League>();
   const [selectedStash, setSelectedStash] = useState<StashTab>();
+
+  useEffect(() => {
+    localStorage.setItem('app.savedItems', JSON.stringify(savedItems));
+  }, [savedItems]);
 
   return (
     <AppContext.Provider
       value={{
+        savedItems,
+        setSavedItems,
         selectedLeague,
         setSelectedLeague,
         selectedStash,
--- src/hooks/useAppContext.ts
+++ src/hooks/useAppContext.ts
@@ -3,13 +3,16 @@ import { createContext, Dispatch, SetStateAction, useContext } from 'react';
 import { League, StashTab } from '../types';
 
 export interface IAppContext {
+  savedItems?: string[];
   selectedLeague?: League;
   selectedStash?: StashTab;
+  setSavedItems: Dispatch<SetStateAction<string[] | undefined>>;
   setSelectedLeague: Dispatch<SetStateAction<League | undefined>>;
   setSelectedStash: Dispatch<SetStateAction<StashTab | undefined>>;
 }
 
 export const AppContext = createContext<IAppContext>({
+  setSavedItems: () => {},
   setSelectedLeague: () => {},
   setSelectedStash: () => {}
 });
